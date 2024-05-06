5ed9b8109c20f7aa5e721545867db4c6657921a5
Evgeny Krivov <g0022@inbox.ru>
2024-05-06 16:27:10 +0300

    First task solution

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main() {
-    setlocale(LC_ALL, "");
-    double num1, num2, result;
-    printf("The first number: ");
-    scanf("%lf", &num1);
-    printf("Second number: ");
-    scanf("%lf", &num2);
+    int num1, num2, result;
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+    printf("Enter a second integer: ");
+    scanf("%d", &num2);
 
     if (num2 == 0) {
         printf("Error: division by zero is not possible!\n");
     } else {
         result = num1 / num2;
-        printf("Division result %.2lf on %.2lf: %.2lf\n", num1, num2, result);
+        printf("The result of dividing %d by %d: %d\n", num1, num2, result);
     }
 
     return 0;
